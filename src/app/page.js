80ebf73513bf50381dec1d5fac7557c05ba87438
Castro <castrojonathand@gmail.com>
2023-11-23 23:53:09 -0300

    add footer

--- src/app/page.js
+++ src/app/page.js
@@ -41,11 +41,7 @@ const Home = () => {
             </div> */}
           </div>
         </div>
-      </section>
-
-      <footer className="bg-gray-800 text-white text-center py-4">
-        <p>© 2023 Portafolio de Desarrollador | Hecho con amor y código</p>
-      </footer>
+      </section>      
 
     </div>
   );
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white text-center py-4">
+        <p>© 2023 Portafolio de Desarrollador | Hecho con amor y código</p>
+        </footer>
+    );
+}
+
+export default footer
