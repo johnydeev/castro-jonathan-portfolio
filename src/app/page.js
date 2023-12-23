c6179b406ba85f2a9c4e3e889d02634931076e9e
Castro <castrojonathand@gmail.com>
2023-12-23 15:15:42 -0300

    to deploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
 "use client"
-import Body from '@/components/Body';
+
+import Contact from '@/components/Contact';
+import Hero from '@/components/Hero';
+import Proyects from '@/components/Proyects';
+import Stack from '@/components/Stack';
 import React from 'react';
 
 const Home = () => {
   return (
     <div>
-      <Body/>
+      <Hero />
+        <Stack />
+        <Proyects />
+        <Contact />
     </div>
     
   );
