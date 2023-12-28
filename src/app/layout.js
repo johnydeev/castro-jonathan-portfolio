59f902273de953cdd69c02643d2725d4c4b20ebd
Castro <castrojonathand@gmail.com>
2023-12-28 00:08:09 -0300

    clean code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import Navbar from '@/components/navbar'
 import './globals.css'
 import Footer from '@/components/footer'
-import Header from '@/components/Header'
+
 
 export const metadata = {
   title: '👨‍💻Portfolio Castro Jonathan',
@@ -11,7 +11,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body className='min-w-min min-h-min'>
-        {/* <Header/> */}
         <Navbar/>
         {children}
         <Footer/>    
