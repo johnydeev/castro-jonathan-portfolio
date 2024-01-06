48ea283273b727ba14b543a33bf3e263a8da8120
Castro <castrojonathand@gmail.com>
2024-01-06 11:02:16 -0300

    fix layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className='min-w-min min-h-min'>
+      <body className='min-h-min'>
         <Navbar/>
         {children}
         <Footer/>    
