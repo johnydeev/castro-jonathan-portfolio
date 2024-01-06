c51bd93f56a32943c759e9060faef8596d08f89d
Castro <castrojonathand@gmail.com>
2024-01-05 22:42:30 -0300

    fix language

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
+    <html lang="en">
       <body className='min-w-min'>
         <Navbar/>
         {children}
