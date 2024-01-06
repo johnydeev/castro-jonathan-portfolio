9162b913f3059b20b4a1c10ad0286ffe29bcf664
Castro <castrojonathand@gmail.com>
2024-01-06 11:29:16 -0300

    Changes

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className='mx-auto'>
+    <div className='lg:w-3/4 mx-auto'>
       <Hero />
       <Stack />
       <Proyects />
