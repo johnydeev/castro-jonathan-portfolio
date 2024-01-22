836e9ecdec91b0b8fb488e5e320e5d67d435053c
Castro <castrojonathand@gmail.com>
2024-01-22 14:58:28 -0300

    remove resend

--- src/app/api/resend/route.js
+++ src/app/api/resend/route.js
@@ -1,23 +1,23 @@
-import { Resend } from 'resend';
-import { NextResponse } from 'next/server';
-import { EmailTemplate } from '@/components/email-template';
+// import { Resend } from 'resend';
+// import { NextResponse } from 'next/server';
+// import { EmailTemplate } from '@/components/email-template';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-    try {
-        const data = await resend.emails.send({
-            from: 'onboarding@resend.dev',
-            to: 'castrojonathand1989@gmail.com',
-            subject: 'Waitlist',
-            html: EmailTemplate,
-            text: '',
+// export async function POST() {
+//     try {
+//         const data = await resend.emails.send({
+//             from: 'onboarding@resend.dev',
+//             to: 'castrojonathand1989@gmail.com',
+//             subject: 'Waitlist',
+//             html: EmailTemplate,
+//             text: '',
             
-        });
-        console.log("data>>",data)
+//         });
+//         console.log("data>>",data)
 
-        return NextResponse.json({message:"Mensaje enviado!"},{status:200})
-    } catch (error) {
-        return Response.json("hubo un error",{ error });
-    }
-}
+//         return NextResponse.json({message:"Mensaje enviado!"},{status:200})
+//     } catch (error) {
+//         return Response.json("hubo un error",{ error });
+//     }
+// }
