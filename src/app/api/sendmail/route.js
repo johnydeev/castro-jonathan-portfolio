88bbc17d79a4efd530863fcde597115497dfa479
Castro <castrojonathand@gmail.com>
2024-05-20 05:46:30 -0300

    Prod

--- src/app/api/sendmail/route.js
+++ src/app/api/sendmail/route.js
@@ -3,44 +3,44 @@ import { transporter } from "@/utils/sendEmail";
 import path from "path";
 
 export async function POST(request) {
-  const { name, email, message } = await request.json();
-  console.log("name>>>", name);
-  console.log("email>>>", email);
-  console.log("message>>>", message);
-  try {
-    if (!name || !email) {
-      return NextResponse.json({ message: "Faltan datos" });
-    }
-    console.log("Mail a enviar>>", email);
+    const { name, email, message } = await request.json();
+    console.log("name>>>", name);
+    console.log("email>>>", email);
+    console.log("message>>>", message);
+    try {
+        if (!name || !email) {
+        return NextResponse.json({ message: "Faltan datos" });
+        }
+        console.log("Mail a enviar>>", email);
 
-    const attachmentPath = path.join(
-      process.cwd(),
-      "public",
-      "CV Jonathan Castro - Desarrollador Full Stack.pdf"
-    );
-    const response = await transporter.sendMail({
-      from: process.env.NEXT_PUBLIC_USER,
-      to: email,
-      subject: "Johnydeev",
-      text: `Hola ${name}! Espero que te encuentres bien, en primer lugar te agradezco por confiar en mi portfolio web que hice con mucho esfuerzo y ❤, segundo.. en breve estare en contacto para charlar acerca de tu propuesta. Gracias!\n\nPD: Adjunto mi CV`,
-      attachments: {
-        path: attachmentPath,
-        contentType: "application/pdf",
-      },
-    });
-    const avisoDeContacto = await transporter.sendMail({
-      from: process.env.NEXT_PUBLIC_USER,
-      to: process.env.NEXT_PUBLIC_USER,
-      subject: email,
-      text: message,
-    });
-    console.log("envio exitoso", response);
-    console.log("AvisoDeContacto>>", avisoDeContacto);
-    return NextResponse.json("successful shipment");
-  } catch (error) {
-    console.log("errorRouteSendEmail>>>", error);
-    return NextResponse.json(error);
-  }
+        const attachmentPath = path.join(
+        process.cwd(),
+        "public",
+        "CV Jonathan Castro - Desarrollador Full Stack.pdf"
+        );
+        const response = await transporter.sendMail({
+        from: process.env.USER,
+        to: email,
+        subject: "Johnydeev",
+        text: `Hola ${name}! Espero que te encuentres bien, en primer lugar te agradezco por confiar en mi portfolio web que hice con mucho esfuerzo y ❤, segundo.. en breve estare en contacto para charlar acerca de tu propuesta. Gracias!\n\nPD: Adjunto mi CV`,
+        attachments: {
+            path: attachmentPath,
+            contentType: "application/pdf",
+        },
+        });
+        const avisoDeContacto = await transporter.sendMail({
+        from: process.env.USER,
+        to: process.env.USER,
+        subject: email,
+        text: message,
+        });
+        console.log("envio exitoso", response);
+        console.log("AvisoDeContacto>>", avisoDeContacto);
+        return NextResponse.json("successful shipment");
+    } catch (error) {
+        console.log("errorRouteSendEmail>>>", error);
+        return NextResponse.json(error);
+    }
 }
 // import { promises as fs } from 'fs';
 
