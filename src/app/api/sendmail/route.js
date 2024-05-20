474c59b41bb9508e7d5157b63e0529f8fd9e7cd6
Castro <castrojonathand@gmail.com>
2024-05-20 05:26:05 -0300

    aver

--- src/app/api/sendmail/route.js
+++ src/app/api/sendmail/route.js
@@ -18,7 +18,7 @@ export async function POST(request){
             subject: "Johnydeev",
             text: `Hola ${name}! Espero que te encuentres bien, en primer lugar te agradezco por confiar en mi portfolio web que hice con mucho esfuerzo y ❤, segundo.. en breve estare en contacto para charlar acerca de tu propuesta. Gracias!\n\nPD: Adjunto mi CV`,
             attachments: {
-                    path: "C:/Users/jony/Desktop/Repos/castro-jonathan-portfolio/public/CV Jonathan Castro - Desarrollador Full Stack.pdf",
+                    path: "public/CV Jonathan Castro - Desarrollador Full Stack.pdf",
                 contentType: "application/pdf",
             },
         });
