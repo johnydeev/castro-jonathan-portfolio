c30a7982545cc7bedcb183fb36cc112dd1164804
Castro <castrojonathand@gmail.com>
2024-05-20 00:50:43 -0300

    env fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
   env: {
     NEXT_PUBLIC: process.env.NEXT_PUBLIC,
-    USER: process.env.USER,
-    PASS: process.env.PASS,
-    DATABASE_URL: process.env.DATABASE_URL
+    NEXT_PUBLIC_USER: process.env.NEXT_PUBLIC_USER,
+    NEXT_PUBLIC_PASS: process.env.NEXT_PUBLIC_PASS,
+    NEXT_PUBLIC_DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,
   },
   images: {
     remotePatterns: [
