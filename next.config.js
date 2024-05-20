753f6bfc3b6ea1e2b2defb848a549234ffbd70c0
Castro <castrojonathand@gmail.com>
2024-05-19 21:58:40 -0300

    fix nextConfig

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    env: {
-      USER: process.env.USER,
-      PASS: process.env.PASS,
-      DATABASE_URL: process.env.DATABASE_URL,
-    },
 
+  env: {
+    USER: process.env.USER,
+    PASS: process.env.PASS,
+    DATABASE_URL: process.env.DATABASE_URL
+  },
+  images: {
     remotePatterns: [
       {
         protocol: "https",
